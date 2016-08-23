f4aa813c7237e6bfe1f47b0334adc9324e10848d
ravenusmc <mcuddy77@gmail.com>
2016-08-23 13:40:04 -0400

    finished main menu function and added a validations file

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 #This is the main file of the project. 
+#Importing the libraries that will be used:
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+#using other files which I wrote for this program. 
+from valid import *
+
+#The main function which will launch the program. 
+def main():
+  print("\033c")
+  print("---------------------------------------------")
+  print("-----    Welcome to drinking data       -----")
+  print("Where you see what countries drink the most!")
+  print("{___________________________________________}")
+  choice = input("Do you want to use the program?(y/n) ")
+  while not mainValid(choice):
+    choice = input("Do you want to use the program?(y/n) ")
+  if choice == "y":
+    mainMenu()
+  elif choice == 'n':
+    print("Thank you for using the program.")
+    print("I hope that you use it soon!")
+
+#This function is where I will actually pull the drinks data as well as let the user choose what 
+#they want to do. 
+def mainMenu():
+  print("\033c")
+  drinks = pd.read_csv('drinks.csv')
+  print(drinks.head())
+
+main()
---
+++ valid.py
@@ -0,0 +1,6 @@
+#All of the functions which I use for validation will be kept in here. 
+def mainValid(choice):
+  if choice == "y" or choice == "n":
+    return True
+  else:
+    return False
